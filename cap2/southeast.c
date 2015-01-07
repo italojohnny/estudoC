392538fd983fb5bb9653bdf9ccc2f9aa46ebd0e5
italojohnny <italojohnnydosanjos@gmail.com>
2015-01-07 15:01:18 -0200

    acrescentado utilizacao de ponteiros

--- cap2/southeast.c
+++ cap2/southeast.c
@@ -1,14 +1,17 @@
 /*
  * Aprendendo sobre ponteiros
+ * Inicialmente era passado para a funcao as copias dos valores das variaveis originais, latitude e longitude
+ * mas dessa forma as variaveis originais não tinham seus valores alterados, apenas as variaveis lat e lon
+ * para resolver isso, deve ser usado ponteiros.
  * 
  */
 
 #include <stdio.h>
 
-void go_south_east (int lat, int lon)//essas sao outras variaveis que receberão cópias dos valores das variaveis originais
+void go_south_east (int* lat, int* lon)//agora a funcao recebe o endereco das variaveis originais
 {
-	lat = lat - 1;//quando modificadas, apenas os valores das copias serao modificados, e nao os valores das variaveis originais
-	lon = lon + 1;
+	*lat = *lat - 1;//as alterações são feitas nas variaveis originais atraves do enderecos delas
+	*lon = *lon + 1;
 }
 
 int main (void)
@@ -16,7 +19,7 @@ int main (void)
 	int latitude = 32;//essas são as variaveis originais
 	int longitude = -64;
 
-	go_south_east(latitude, longitude);//passando copias dos valores das variaveis originais
+	go_south_east(&latitude, &longitude);//passando os endereco das variaveis originais e possivel alterar de outra funcao, os valores originais
 
 	printf("Avast! Now at: [%i, %i]\n", latitude, longitude);
 
