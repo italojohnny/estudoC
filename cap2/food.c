91f65fd967670cc270a216e0cd2389a8f714784f
italojohnny <italojohnnydosanjos@gmail.com>
2015-01-08 11:36:43 -0200

    fgets() e uma alternativa para scanf()

--- cap2/food.c
+++ cap2/food.c
@@ -4,7 +4,8 @@ int main (void)
 {
 	char food[5];
 	printf("Enter favorite food: ");
-	scanf("%s", food);//Segmentation fault, caso o que o usuario escrever exceda a capacidade da array
+	//scanf("%s", food);//Segmentation fault, caso o que o usuario escrever exceda a capacidade da array
+	fgets(food, sizeof(food), stdin);//fgets() e uma alternativa para scanf()
 	printf("Favorite food: %s\n", food);
 
 	return 0;
