23f51eab7d20374a8610f7ac8760a1d9ea405ea6
italojohnny <italojohnnydosanjos@gmail.com>
2015-01-09 12:55:26 -0200

    comecando capitulo 4

--- cap3/categorize.c
+++ cap3/categorize.c
@@ -19,7 +19,11 @@ int main (int argc, char *argv[])
         return 1;
     }
 
-    FILE *in = fopen("spooky.csv", "r");
+    FILE *in;
+    if (!(in = fopen("spooky.csv", "r"))) {
+        fprintf(stderr, "Can't open the file.\n");
+        return 1;
+    }        
     FILE *file1 = fopen(argv[2], "w");
     FILE *file2 = fopen(argv[4], "w");
     FILE *file3 = fopen(argv[5], "w");
