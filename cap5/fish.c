143b86a5fa39e46c1db5082cbede3f822f3c5455
italojohnny <italojohnnydosanjos@gmail.com>
2015-01-12 14:00:53 -0200

    refatoracao do codigo, struct dentro de struct

--- cap5/fish.c
+++ cap5/fish.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
-
+struct preferences {
+    const char *food;
+    float exercise_hours;
+};
 struct fish {
     const char *name;
     const char *species;
     int teeth;
     int age;
+    struct preferences care;
 };
+void preferences (struct fish f)
+{
+    printf("%s like to eat %s\n", f.name, f.care.food);
+    printf("%s like to exercise for %f hours\n", f.name,
+    f.care.exercise_hours);
+}
 
 void catalog (struct fish f)
 {
@@ -21,9 +31,9 @@ void label (struct fish f)
 
 int main ()
 {
-    struct fish snappy = {"Snappy", "Piranha", 69, 4};
+    struct fish snappy = {"Snappy", "Piranha", 69, 4, {"Meat", 7.5}};
     catalog(snappy);
     label(snappy);
-
+    preferences(snappy);
     return 0;    
 }
