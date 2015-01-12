3c818305598cfca955e214900fa4430e8e743381
italojohnny <italojohnnydosanjos@gmail.com>
2015-01-12 15:15:53 -0200

    resolucao do problema em passar struct por parametro

--- cap5/turtle.c
+++ cap5/turtle.c
@@ -13,16 +13,16 @@ typedef struct {
     int age;
 } turtle;
 
-void happy_birthday (turtle t)
+void happy_birthday (turtle *t)
 {
-    t.age = t.age + 1;
-    printf("Happy birthday %s! You are now %i years old!\n", t.name, t.age);
+    (*t).age = (*t).age + 1;
+    printf("Happy birthday %s! You are now %i years old!\n", (*t).name, (*t).age);
 }
 
 int main (void)
 {
     turtle myrtle = {"Myrtle", "Leatherback sea turtle", 99};
-    happy_birthday(myrtle);
+    happy_birthday(&myrtle);
     printf("Happy birthday %s! You are now %i years old!\n", myrtle.name,
     myrtle.age);
     return 0;
