b39fe91440842d595a8651ba96051535c59a8bb6
italojohnny <italojohnnydosanjos@gmail.com>
2015-01-17 11:38:50 -0200

    usando uma array de ponteiros para funcoes

--- cap7/send_dear_johns.c
+++ cap7/send_dear_johns.c
@@ -1,19 +1,9 @@
-#include <stdio.h>
+/*
+    Array de ponteiros para funçoes
+*/
+#include "send.h"
 
-enum response_type {
-    DUMP,
-    SECOND_CHANCE,                   
-    MARRIAGE
-};
-
-typedef struct {
-    char *name;
-    enum response_type type;
-} response;
-
-void dump (response);
-void second_chance (response);
-void marriage (response);
+void (*replies[])(response) = {dump, second_chance, marriage};
 
 int main (void)
 {
@@ -25,37 +15,8 @@ int main (void)
     };
     int i;
     for (i = 0; i < 4; i++) {
-        switch (r[i].type) {
-            case DUMP:  dump(r[i]);
-            break;
-            case SECOND_CHANCE: second_chance(r[i]);
-            break;
-            default: marriage(r[i]);
-        }
+        (replies[r[i].type])(r[i]);
     }
     return 0;
 }
 
-void dump (response r)
-{
-    printf("Dear %s,\n", r.name);
-    puts("Unfortunately your last date contacted us to");
-    puts("say that they will not be seeing you again");
-    printf("\n");
-}
-
-void second_chance (response r)
-{
-    printf("Dear %s,\n", r.name);
-    puts("Good news: your last date has asked us to");
-    puts("arrange another meeting. Please call ASAP.");
-    printf("\n");
-}
-
-void marriage (response r)
-{
-    printf("Dear %s,\n");
-    puts("Congratulations! Your last date has contacted");
-    puts("us with a proposal of marriage.");
-    printf("\n");
-}
