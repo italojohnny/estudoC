1232546d7d5e8a97ba4e439a449e714d2c42e3a5
italojohnny <italojohnnydosanjos@gmail.com>
2015-01-19 13:46:02 -0200

    refatoracao, continuando com estudo de ligacao dinamica

--- cap8/biblioteca_dinamica/elliptical.c
+++ cap8/biblioteca_dinamica/elliptical.c
@@ -1,3 +1,25 @@
+/*
+   
+    gcc -I./includes -c hfcal.c -o hfcal.o
+    gcc -I./includes -c elliptical.c -o elliptical.o
+    
+            //ligação estática 
+            ar -rcs ./libs/libhfcal.a hfcal.o
+            gcc elliptical.o -L./libs -lhfcal -o elliptical
+            ./elliptical
+
+                //código independente de posição
+                gcc -I./includes -fPIC -c hfcal.c -o hfcal.o
+
+            //ligação dinâmica
+            gcc -shared hfcal.o -o ./libs/libhfcal.so
+            gcc elliptical.o -L./libs -lhfcal -o elliptical 
+            export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./libs
+            ./elliptical
+*/
+
+
+
 #include <stdio.h>
 #include <hfcal.h>
 
