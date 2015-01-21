de039b59eb07f4b071f34333c2888a0bbfecd568
italojohnny <italojohnnydosanjos@gmail.com>
2015-01-21 20:22:28 -0200

    estudando pipe()

--- cap10/news_opener.c
+++ cap10/news_opener.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 void error (char*);
 void open_url (char*);
@@ -9,31 +10,22 @@ int main (int argc, char *argv[])
     char *phrase = argv[1];//recebe o primeiro argumeto passado a este programa
     char *vars[] = {"RSS_FEED=http://feeds.bbci.co.uk/news/rss.xml", NULL};//local onde serao lido as noticias
     int fd[2];//essa array vai armazenar os descritores para o seu pipe;
-
-    _
-    _
-    _
-
+    
+    if (pipe(fd) == -1) error("Can't create the pipe");
     pid_t pid = fork();//cria um processo filho
-    if (pid == 1) {//verifica se o atual processo ...
-        error("Can't fork process");
-    }
+    if (pid == -1) error("Can't fork process");
 
     if (!pid) {//verifica se o atual processo ...
-        _
-        _
-        if (execle( == -1)) {
-            error("Can't rum script");
-        }
+        dup2(fd[1], 1);
+        close(fd[0]);
+        if (execle("/usr/bin/python", "/usr/bin/python", "./rssgossip.py", "-u", phrase, NULL, vars) == -1) error("Can't rum script");
     }
-    _
-    _
+    dup2(fd[0], 0);
+    close(fd[1]);
 
     char line[255];
-    while (fgets(line, 255, __)) {
-        if (line[0] == '\t')
-            open_url(line + 1);
-    }
+    while (fgets(line, 255, stdin))
+        if (line[0] == '\t') open_url(line + 1);
 
    return 0;
 }
@@ -46,6 +38,16 @@ void error (char *msg)
 
 void open_url (char *url)
 {
-    if (1) 
-        error("Can't open url");
+    /*
+    FILE *file = fopen("urls.txt", "w");
+    if (!file) error("nao deu pra abrir o arquivo");
+    fprintf(file, "%s\n", url);
+    fclose(file);
+    */
+    char launch[255];
+    pid_t pid = fork();
+    if (pid == -1) error("Can't fork process");
+    if (!pid) {
+        if (execlp("firefox", "firefox", url, NULL) == -1) error("Can't run navigator");
+    }
 }
