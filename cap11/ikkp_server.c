68035c934db9d5bd49f737fcc6313be653e50710
italojohnny <italojohnnydosanjos@gmail.com>
2015-01-23 00:32:31 -0200

    adicionando multi-processos para execucao do servico

--- cap11/ikkp_server.c
+++ cap11/ikkp_server.c
@@ -37,19 +37,26 @@ int main (int argc, char *argv[])
         int connect_d = accept(listener_d, (struct sockaddr*)&client_addr, &address_size);
         if (connect_d == -1)
             error("Can't open secondary socket");
-        if (say(connect_d, "Internet Knock-Knock Protocol Serve\r\nVersion 1.0\r\nKnock! Knock!\r\n> ") != -1) {
-            read_in(connect_d, buf, sizeof(buf));
-            if (strncasecmp("Who's there?", buf, 12))
-                say(connect_d, "You should say 'Who's there?'!");
-            else {
-                if (say(connect_d, "Oscar\r\n> ") != -1) {
-                    read_in(connect_d, buf, sizeof(buf));
-                    if (strncasecmp("Oscar who?", buf, 10))
-                        say(connect_d, "You should say 'Oscar who?'!\r\n");
-                    else
-                        say(connect_d, "Oscar silly question, you get a silly answer\r\n");
+
+        if (!fork()) {
+            close(listener_d);
+
+            if (say(connect_d, "Internet Knock-Knock Protocol Serve\r\nVersion 1.0\r\nKnock! Knock!\r\n> ") != -1) {
+                read_in(connect_d, buf, sizeof(buf));
+                if (strncasecmp("Who's there?", buf, 12))
+                    say(connect_d, "You should say 'Who's there?'!");
+                else {
+                    if (say(connect_d, "Oscar\r\n> ") != -1) {
+                        read_in(connect_d, buf, sizeof(buf));
+                        if (strncasecmp("Oscar who?", buf, 10))
+                            say(connect_d, "You should say 'Oscar who?'!\r\n");
+                        else
+                            say(connect_d, "Oscar silly question, you get a silly answer\r\n");
+                    }
                 }
             }
+            close(connect_d);
+            exit(0);
         }
         close(connect_d);
     }
