335f31b90d758819617f45e4498b4917b83de38d
italojohnny <italojohnnydosanjos@gmail.com>
2015-01-23 11:17:38 -0200

    finalizado capitulo 11

--- cap11/wiki_client.c
+++ cap11/wiki_client.c
@@ -13,7 +13,21 @@ int say (int, char*);
 
 int main (int argc, char *argv[])
 {
-    puts("ola mundo");
+    int d_sock;
+    d_sock = open_socket("en.wikipedia.org", "80");
+    char buf[255];
+    sprintf(buf, "GET /wiki/%s http/1.1\r\n", argv[1]);
+    say(d_sock, "Host: en.wikipedia.org\r\n\r\n");
+    char rec[256];
+    int bytesRcvd = recv(d_sock, rec, 255, 0);
+    while (bytesRcvd) {
+        if(bytesRcvd == -1)
+            error("Can't read from server");
+        rec[bytesRcvd] = '\0';
+        printf("%s", rec);
+        bytesRcvd = recv(d_sock, rec, 255, 0);
+    }
+    close(d_sock);
     return 0;
 }
 
