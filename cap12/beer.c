c62804d02186b529061e43f8fda7d97294d44811
italojohnny <italojohnnydosanjos@gmail.com>
2015-01-23 17:36:40 -0200

    finalizado capitulo 12

--- cap12/beer.c
+++ cap12/beer.c
@@ -1,19 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
+//#include <unistd.h>
 #include <errno.h>
 #include <pthread.h>
 
 int beers = 2000000;
 
+pthread_mutex_t beers_lock = PTHREAD_MUTEX_INITIALIZER;
+/*
+Versio A
+void* drink_lots(void *a)
+{
+    int i;
+    pthread_mutex_lock(&beers_lock);
+    for (i = 0; i < 1000000; i++) {
+        beers = beers - 1;
+    }
+    pthread_mutex_unlock(&beers_lock);
+    printf("beers = %i\n", beers);
+    return NULL;
+}
+*/
+//versio B
 void* drink_lots(void *a)
 {
     int i;
     for (i = 0; i < 1000000; i++) {
+        pthread_mutex_lock(&beers_lock);
         beers = beers - 1;
+        pthread_mutex_unlock(&beers_lock);
     }
+     
+    printf("beers = %i\n", beers);
+
     return NULL;
+
 }
 
 int main (void)
