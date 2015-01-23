d3bdb666bcde7830222fa218b797eab92e84cdda
italojohnny <italojohnnydosanjos@gmail.com>
2015-01-23 21:59:00 -0200

    reorganizacao de pastas e introducao dos clabs

---
+++ livro/clab2/main.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
+
+int
+main(int argc, char *argv[])
+{
+    IplImage *img;
+
+    if (argc != 2) {
+        puts("Usage: cinza file");
+        return EXIT_FAILURE;
+    }
+
+    img = cvLoadImage(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
+
+    if (img == NULL) {
+        puts("Could not load image file.");
+        return EXIT_FAILURE;
+    }
+
+    cvSaveImage("cinza.png", img, 0);
+
+    cvReleaseImage(&img);
+    return EXIT_SUCCESS;
+}
