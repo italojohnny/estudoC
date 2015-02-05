b4fdf3100e4991af1feac71fea544ad16154af5d
italojohnny <italojohnnydosanjos@gmail.com>
2015-02-05 14:44:05 -0200

    refatoracao do estudo de lista dinamica, acrescimo de novas funcoes

---
+++ edd/01-lista/list.h
@@ -0,0 +1,137 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct List {
+    int info;
+    struct List *next;
+} List;
+
+void insert (List **L, int x)
+{
+    List *P = malloc(sizeof(List));
+    P->info = x;
+    P->next = *L;
+    *L = P;
+}
+
+List* insert2 (List *L, int x)
+{
+    List *P = malloc(sizeof(List));
+    P->info = x;
+    P->next = L;
+    return P;
+}
+
+List* insert_last (List *L, int x)
+{
+    List *Q, *P = malloc(sizeof(List));
+    P->info = x;
+    P->next = NULL;
+    if (L == NULL)
+        return P;
+    else {
+        Q = L;
+        while (Q->next != NULL)
+            Q = Q->next;
+        Q->next = P;
+        return L;
+    }
+}
+
+void insert_order (List **L, int x)
+{
+    List *percorre = *L, *aux = NULL, *P = malloc(sizeof(List));
+    P->info = x;
+    while (percorre != NULL && x > percorre->info) {
+        aux = percorre;
+        percorre = percorre->next;
+    }
+    if (aux == NULL) {
+        P->next = *L;
+        *L = P;
+    } else {
+        P->next = aux->next;
+        aux->next = P;
+    }
+}
+
+List* _remove (List *L)
+{
+    if (L == NULL) return NULL;
+    
+    List *aux = L;
+    printf("\n %d removed", aux->info);
+    L = aux->next;
+    free(aux);
+    return L;
+}
+
+int _remove2 (List **L)
+{
+    if (*L == NULL) return 0;
+
+    int x;
+    List *aux = *L;
+    x = aux->info;
+    *L = aux->next;
+    free(aux);
+    return x;
+}
+
+void _remove_all (List **L, int x)
+{
+    List *percorre, *aux, *P;
+    percorre = *L;
+    aux = NULL;
+    while (percorre != NULL) {
+        if (percorre->info == x) {
+            if (aux == NULL) {
+                P = *L;
+                *L = P->next;
+                free(P);
+                percorre = *L;
+            } else {
+                percorre = percorre->next;
+                P = aux->next;
+                aux->next = P->next;
+                free(P);
+            }
+        } else {
+            aux = percorre;
+            percorre = percorre->next;
+        }
+    }
+}
+
+void concatena (List **L, List *L2)
+{
+    List *P = *L;
+    while (P->next != NULL)
+        P = P->next;
+    P->next = L2;
+}
+
+void show (List *L)
+{
+    List *P = L;
+    while (P != NULL) {
+        printf(" |%d| ", P->info);
+        P = P->next;
+    }    
+}
+
+int show_last (List *L)
+{
+    List *Q = L;
+    while (Q->next != NULL)
+        Q = Q->next;
+    return Q->info;
+}
+
+void show_rec (List *L)
+{
+    if (L != NULL) {
+        show_rec(L->next);
+        printf(" |%d| ", L->info);
+    }
+}
--- edd/01-lista/main.c
+++ edd/01-lista/main.c
@@ -1,37 +1,66 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-typedef struct List {
-    int info;
-    struct List *next;
-} List;
+#include "list.h"
 
 int main (void)
 {
-    List *L = NULL, *P;
+    List *L = NULL, *L2 = NULL;
     int op, x;
     do {
-        printf("\n1- Insert into list");
-        printf("\n2- Show list");
+        show(L);
+        printf("\n1 - Insert");
+        printf("\n2 - Insert(mode2)");
+        printf("\n3 - Insert last");
+        printf("\n4 - Insert order");
+        printf("\n5 - Remove");
+        printf("\n6 - Remove (mode2)");
+        printf("\n7 - Remove all");
+        printf("\n8 - Concatenate");
+        printf("\n9 - Show");
+        printf("\n10- Show last");
+        printf("\n11- Show (recursive)");
         printf("\n0- Exit\n> ");
         scanf("%d", &op);
 
         switch (op) {
-            case 1:
-                printf("choose a number ");
-                scanf("%d", &x);
-                P = malloc(sizeof(List));
-                P->info = x;
-                P->next = L;
-                L = P;
+            case 0: break;
+            case 1: printf("\nchoose a number: ");
+                    scanf("%d", &x);
+                    insert(&L, x);                        
             break;
-            case 2: 
-                P = L;
-                while (P != NULL) {
-                    printf(" |%d| ", P->info);
-                    P = P->next;
-                }
+            case 2: printf("\nchoose a number: ");
+                    scanf("%d", &x);
+                    L = insert2(L, x);
             break;
+            case 3: printf("\nchoose a number: ");
+                    scanf("%d", &x);
+                    L = insert_last(L, x);                    
+            break;
+            case 4: printf("\nchoose a number: ");
+                    scanf("%d", &x);
+                    insert_order(&L, x);
+            break;
+            case 5: L = _remove(L);
+            break;
+            case 6: printf("\n%d removed", _remove2(&L));
+            break;
+            case 7: printf("\nchoose a number");
+                    scanf("%d", &x);
+                    _remove_all(&L, x);
+            break;
+            case 8: concatena(&L, L2);
+            break;
+            case 9: show(L);
+            break;
+            case 10: printf("%d is the last", show_last(L));
+            break;
+            case 11:printf("\n");
+                    show_rec(L);
+            break;
+            default:insert_order(&L2, 12);
+                    insert_order(&L2, 23);
+                    insert_order(&L2, 666);
+                    insert_order(&L2, 1);
+                    insert_order(&L2, 16);
+                    show_rec(L2);                
         }
     } while (op != 0);
     return 0;
