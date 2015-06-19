6132dc1251c775bfaefb335aa78589601dc39861
Italo Johnny <italojohnnydosanjos@gmail.com>
2015-06-19 13:36:55 -0300

    comecando capitulo 7

---
+++ apst/cap07/01_capturar_pontos.c
@@ -0,0 +1,17 @@
+/* Captura e imprime as coordenadas de um ponto qualquer */
+#include <stdio.h>
+
+struct ponto {
+	float x;
+	float y;
+};
+
+int main (void)
+{
+	struct ponto p;
+	printf("Digite as coordenadas do ponto(x, y): ");
+	scanf("%f %f", &p.x, &p.y);
+	printf("O ponto fornecido foi: (%.2f, %.2f)\n", p.x, p.y);
+
+	return 0;
+}
